87450c9f7b94f61f8866057639a39cd3c8c681af
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-17 17:36:40 -0700

    Moving code to print to-do items in list to a separate function for re-usability

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const todoList = [];
 const addTodo = function () {
     const item = document.getElementById("todo-input").value;
     todoList.push(item);
-    
+    listTodos();
+
+    document.getElementById("todo-input").value = "";
+}
+
+const listTodos = function(){
     document.getElementById("todo-items").innerHTML = "";
     todoList.forEach((item) => {
         document.getElementById("todo-items").innerHTML += `<p>${item}</p>`
     })
-
-    document.getElementById("todo-input").value = "";
 }
