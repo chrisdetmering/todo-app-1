f45404016f7fef5945c4111eedf47300f88582ba
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-19 18:15:34 -0700

    Adding fonts for to-do list styling and importing fonts from google fonts in html file

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const todoList = [];
+let todoList = [];
 
 const addTodo = function () {
     const item = document.getElementById("todo-input").value;
+
     todoList.push(item);
     listTodos();
 
     document.getElementById("todo-input").value = "";
 }
 
+const deleteItem = function (item) {
+    todoList = todoList.filter((value) => value !== item);
+    listTodos();
+}
+
 const listTodos = function(){
     document.getElementById("todo-items").innerHTML = "";
+    let itemCheckbox, itemIcon;
     todoList.forEach((item) => {
-        document.getElementById("todo-items").innerHTML += `<p>${item}</p>`
+        let itemsList = document.getElementById("todo-items");
+        itemCheckbox = `<input type=checkbox name=${item}'>`
+        itemsList.innerHTML += `<div class=item>${itemCheckbox}
+                                    <label for=${item}>${item}</label>
+                                    <i class='fa fa-trash' aria-hidden='true' onclick='deleteItem(${item})'></i>
+                                </div>`
     })
 }
