1a6535eb1ec16293e0188b52d9e14dcca293f71a
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-19 19:34:04 -0700

    Adding comments for script functions

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let todoList = [];
 
+//function to add to-do item to the array then list them out
 const addTodo = function () {
     const inputField = document.getElementById("todo-input");
     const item = inputField.value;
+
+    //validating that user entered text into field, otherwise add red border & exit
     if(item.length === 0){
         inputField.style = 'border: 1px solid red';
         return;
@@ -11,18 +14,24 @@ const addTodo = function () {
     todoList.push(item);
     listTodos();
 
+    //clearing text in input field and reseting border
+    //styling in case it was changed to red
     inputField.value = "";
     inputField.style = 'border: 1px solid darkgray';
 }
 
-const deleteItem = function (item) {
-    todoList = todoList.filter((value) => value !== item);
+//deletes and item from the array then re-lists todo items
+const deleteItem = function (todoItem) {
+    todoList = todoList.filter((value) => value !== todoItem);
     listTodos();
 }
 
-const checkBox = function(item) {
-    const checkbox = document.getElementById(item+"-checkbox");
-    const icon = document.getElementById(item + "-icon");
+//will display trash icon if box is checked
+//or hid icon if box is unchecked
+const checkBox = function(todoItem) {
+    const checkbox = document.getElementById(todoItem+"-checkbox");
+    const icon = document.getElementById(todoItem + "-icon");
+    
     if(checkbox.checked){
         icon.style = 'display: block'
     } else {
@@ -30,8 +39,10 @@ const checkBox = function(item) {
     }
 }
 
+//creates HTML as string for each todo item then appends to todo-items DIV box
 const listTodos = function(){
     document.getElementById("todo-items").innerHTML = "";
+
     todoList.forEach((item) => {
         let itemsList = document.getElementById("todo-items");
         itemsList.innerHTML += `<div class=item-div>
