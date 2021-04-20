194411cab875846730e860069938697ad6fdbce5
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-19 19:19:56 -0700

    setting dynamic id names to the todo-item elements, and adding function to display trash icon if box is checked

--- script.js
+++ script.js
@@ -14,14 +14,24 @@ const deleteItem = function (item) {
     listTodos();
 }
 
+const checkBox = function(item) {
+    const checkbox = document.getElementById(item+"-checkbox");
+    const icon = document.getElementById(item + "-icon");
+    if(checkbox.checked){
+        icon.style = 'display: block'
+    } else {
+        icon.style = 'display: none'
+    }
+}
+
 const listTodos = function(){
     document.getElementById("todo-items").innerHTML = "";
     todoList.forEach((item) => {
         let itemsList = document.getElementById("todo-items");
-        itemsList.innerHTML += `<div class=item>
-                                    <input type=checkbox name=${item}'>
+        itemsList.innerHTML += `<div class=item-div>
+                                    <input type=checkbox id='${item}-checkbox' name=${item} onclick="checkBox('${item}')">
                                     <label for=${item}>${item}</label>
-                                    <i class='fa fa-trash' aria-hidden='true' onclick="deleteItem('${item}')"></i>
+                                    <span><i id='${item}-icon' class='fa fa-trash' style='display: none' aria-hidden='true' onclick="deleteItem('${item}')"></i></span>
                                 </div>`
     })
 }
